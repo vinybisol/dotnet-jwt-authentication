a2298fc01c9f6d7c52f3cf2dfd868bcf72700043
Vinicius Bisol <vinybisol@gmail.com>
2023-11-04 01:16:19 -0300

    remove

--- dotnet-jwt-authentication/Services/TokenService.cs
+++ dotnet-jwt-authentication/Services/TokenService.cs
@@ -3,28 +3,27 @@ using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Text;
 
-namespace dotnet_jwt_authentication.Services
+namespace dotnet_jwt_authentication.Services;
+
+public class TokenService
 {
-    public class TokenService
+    public string TokenGenerate(User user) 
     {
-        public string TokenGenerate(User user) 
+        var handler = new JwtSecurityTokenHandler();
+
+        var key = Encoding.ASCII.GetBytes(Configurations.TokenSecret.PrivateKey);
+
+        var credentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature);
+
+        var tokenDescriptor = new SecurityTokenDescriptor
         {
-            var handler = new JwtSecurityTokenHandler();
+            SigningCredentials = credentials,
+            Expires = DateTime.UtcNow.AddHours(2),
+        };
 
-            var key = Encoding.ASCII.GetBytes(Configurations.TokenSecret.PrivateKey);
+        var token = handler.CreateToken(tokenDescriptor);
 
-            var credentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature);
-
-            var tokenDescriptor = new SecurityTokenDescriptor
-            {
-                SigningCredentials = credentials,
-                Expires = DateTime.UtcNow.AddHours(2),
-            };
-
-            var token = handler.CreateToken(tokenDescriptor);
-
-            var strToken = handler.WriteToken(token);
-            return strToken;
-        }
+        var strToken = handler.WriteToken(token);
+        return strToken;
     }
 }
