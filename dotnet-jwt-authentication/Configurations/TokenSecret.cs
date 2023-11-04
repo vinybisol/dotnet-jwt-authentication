c6b45e531a7d5ef8f05d5e723e1ed1918504ec38
TheOne <TheOne@DESKTOP-F9SEF8V>
2023-11-04 01:13:42 -0300

    Add project files.

---
+++ dotnet-jwt-authentication/Configurations/TokenSecret.cs
@@ -0,0 +1,6 @@
+﻿namespace dotnet_jwt_authentication.Configurations;
+
+public static class TokenSecret
+{
+    public static string PrivateKey => "i77_2sM^VcL3Pi5&4L£n}s98sd7f(*7d?Ua|%I17op!";
+}
---
+++ dotnet-jwt-authentication/Models/User.cs
@@ -0,0 +1,4 @@
+﻿namespace dotnet_jwt_authentication.Models;
+
+public record User(int Id, string Email, string Password, string[] Roles);
+
